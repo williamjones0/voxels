95eeb17ef1d3e92a711add7841071c722eb239b3
William Jones <wmjones3142@gmail.com>
2024-05-27 21:18:18 +0100

    Avoid attempt to access a vector index out of range by clamping the noise value

--- src/util/Util.hpp
+++ src/util/Util.hpp
@@ -3,3 +3,7 @@
 int getVoxelIndex(int x, int y, int z, int size) {
 	return y * size * size + z * size + x;
 }
+
+float clamp(float x, float low, float high) {
+	return std::max(low, std::min(x, high));
+}
--- src/world/Noise.hpp
+++ src/world/Noise.hpp
@@ -4,6 +4,8 @@
 #include <util/PerlinNoise.hpp>
 #include <util/Util.hpp>
 
+const float EPSILON = 0.000001;
+
 void generateTerrain(std::vector<int>& voxels, unsigned int seed, int worldSize, int heightScale) {
     int* chunk = new int[worldSize * worldSize];
     const siv::PerlinNoise::seed_type s = seed;
@@ -11,7 +13,7 @@ void generateTerrain(std::vector<int>& voxels, unsigned int seed, int worldSize,
 
     for (int z = 0; z < worldSize; ++z) {
         for (int x = 0; x < worldSize; ++x) {
-            const double noise = perlin.octave2D_01((x * 0.01), (z * 0.01), 4);
+            const double noise = clamp(perlin.octave2D_01((x * 0.01), (z * 0.01), 4), 0.0, 1.0 - EPSILON);
             const int height = noise * heightScale;
             chunk[z * worldSize + x] = height;
             int index = getVoxelIndex(x, height, z, worldSize);
